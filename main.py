f1cc26d3faf6af1957b33e53eefda0ebaafd7802
AferDust <github.com>
2023-04-30 01:04:51 +0600

    Set up PyGame lib and run window

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('Hello from Main class')
+import pygame, sys
+
+from pygame.locals import *
+pygame.init()
+pygame.display.set_caption('Fireboy and Watergirl')
+
+
+WINDOW_SIZE = (900, 600)
+screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
